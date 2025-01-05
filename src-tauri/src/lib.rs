8b6ef56b6505fd8feb9bcb9c9e0cd2dca35fa168
Azmek <68204002+Azmekk@users.noreply.github.com>
2025-01-05 03:10:51 +0200

    Removed auto update and versioning

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,6 @@
 mod handlers;
 mod utils;
 
-//use extensions::builder_extensions::BuilderExt;
-use tauri_plugin_updater::UpdaterExt;
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
@@ -20,47 +17,9 @@ pub fn run() {
             handlers::updates_handler::check_for_updates,
             handlers::updates_handler::update_app
         ])
-        .setup(|app| {
-            let handle = app.handle().clone();
-            tauri::async_runtime::spawn(async move {
-                println!("Sleeping update thread...");
-                tokio::time::sleep(tokio::time::Duration::from_secs(600)).await;
-                update(handle).await.unwrap();
-            });
-            Ok(())
-        })
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_opener::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-}
-
-async fn update(app: tauri::AppHandle) -> tauri_plugin_updater::Result<()> {
-    if let Some(update) = app
-        .updater_builder()
-        .timeout(std::time::Duration::from_secs(10))
-        .build()?
-        .check()
-        .await?
-    {
-        let mut downloaded = 0;
-
-        update
-            .download_and_install(
-                |chunk_length, content_length| {
-                    downloaded += chunk_length;
-                    println!("downloaded {downloaded} from {content_length:?}");
-                },
-                || {
-                    println!("download finished");
-                },
-            )
-            .await?;
-
-        println!("update installed");
-        app.restart();
-    }
-
-    Ok(())
 }
